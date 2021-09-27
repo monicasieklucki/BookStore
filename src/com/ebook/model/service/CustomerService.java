53f5d6f2d2bca6982760fbd2b12e346a049034ad
Monica Sieklucki <monica@Monicas-MacBook-Air.local>
2021-09-26 20:13:27 -0500

    finished customer service

--- src/com/ebook/model/service/CustomerService.java
+++ src/com/ebook/model/service/CustomerService.java
@@ -2,21 +2,24 @@ package com.ebook.model.service;
 
 import com.ebook.dal.CustomerDAO;
 import com.ebook.model.customer.Customer;
+import com.ebook.model.customer.Address;
 
 public class CustomerService {
 	private CustomerDAO custDAO = new CustomerDAO();
 	
 	//search customer by ID from the DB
 	public Customer findCustomerById(String customerId) {
+		
+		Customer customer = null;
 			
 		try {
-			Customer customer = custDAO.getCustomer(customerId);
+			customer = custDAO.getCustomer(customerId);
 	    	return customer;
 	    } catch (Exception se) {
 	      System.err.println("CustomerService: Threw a Exception retrieving customer.");
 	      System.err.println(se.getMessage());
 	    }
-		return null;
+		return customer;
 	}
 	
 	//Insert a new customer in the DB
@@ -26,6 +29,27 @@ public class CustomerService {
 			custDAO.addCustomer(customer);
 	    } catch (Exception se) {
 	      System.err.println("CustomerService: Threw a Exception retrieving customer.");
+	      System.err.println(se.getMessage());
+	    }
+	}
+	
+	
+	//Delete customer from DB
+	public void removeCustomerById(Customer customer) {
+		
+		try {
+			custDAO.removeCustomer(customer.getCustomerId());
+	    } catch (Exception se) {
+	      System.err.println("CustomerService: Threw a Exception removing customer.");
+	      System.err.println(se.getMessage());
+	    }
+	}
+	//Update customer in DB
+	public void updateCustomer(Customer customer, Address address) {	
+		try {
+			custDAO.updateCustomer(customer, address);
+	    } catch (Exception se) {
+	      System.err.println("CustomerService: Threw a Exception updating customer.");
 	      System.err.println(se.getMessage());
 	    }
 	}
