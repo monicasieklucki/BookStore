106b1d33c26ea8bdf615d4d1dfc0ec43dc07b1c8
Monica Sieklucki <monica@Monicas-MacBook-Air.local>
2021-11-06 18:58:13 -0500

    added vendor resource and activity

--- src/com/ebook/service/vendor/resource/VendorResource.java
+++ src/com/ebook/service/vendor/resource/VendorResource.java
@@ -13,6 +13,8 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.CacheControl;
 
+import com.ebook.model.vendor.Vendor;
+import com.ebook.model.vendor.VendorManager;
 import com.ebook.service.vendor.representation.VendorRepresentation;
 import com.ebook.service.vendor.workflow.VendorActivity;
 
@@ -26,7 +28,39 @@ public class VendorResource {
 	public VendorRepresentation getVendor(@PathParam("vendorId") Integer id) {
 		System.out.println("GET METHOD Request for vendor........");
 		VendorActivity vendActivity = new VendorActivity();
-		return vendActivity.getVendorById(id);
+		return vendActivity.getVendor(id);
 	}
+	
+	@POST
+	@Produces({"application/xml" , "application/json"})
+	@Path("/vendor")
+	public VendorRepresentation addVendor(String vendorName) {
+		
+		Vendor vendor = VendorManager.addVendor(vendorName);
+		
+		VendorRepresentation vendRep = new VendorRepresentation(vendor);
+		return vendRep;
+	}
+	
+	@DELETE
+	@Produces({"application/xml" , "application/json"})
+	@Path("/vendor/{vendorId}")
+	public String removeVendor(Integer vendorId) {
+		
+		VendorManager.removeVendorById(vendorId);
+		
+		return "OK";
+	}
+	
+	/*@POST
+	@Produces({"application/xml" , "application/json"})
+	@Path("/vendor/{vendorId}/{productTitle}/{productPrice}")*/
+	/*public ProductRepresentation addVendorProduct(Integer vendorId, String productTitle, double productPrice) {
+		
+		Product product = VendorManager.addVendorProduct(vendorId, productTitle, productPrice)
+		
+		ProductRepresentation prodRep = new ProductRepresentation(product);
+		return prodRep;
+	}*/
 	
 }
--- src/com/ebook/service/vendor/workflow/VendorActivity.java
+++ src/com/ebook/service/vendor/workflow/VendorActivity.java
@@ -7,15 +7,42 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import com.ebook.model.customer.Customer;
+import com.ebook.model.customer.CustomerManager;
 import com.ebook.model.vendor.Vendor;
 import com.ebook.model.vendor.VendorManager;
+import com.ebook.service.representation.CustomerRepresentation;
 import com.ebook.service.vendor.representation.VendorRepresentation;
 
 
 public class VendorActivity {
-
-	public VendorRepresentation getVendorById(Integer id){
+	
+	public VendorRepresentation getVendor(Integer id){
+		
 		Vendor vendor = VendorManager.getVendorById(id);
 		return new VendorRepresentation(vendor);
 	}
+	
+	public VendorRepresentation addVendor(String vendorName) {
+		
+		Vendor vendor = VendorManager.addVendor(vendorName);
+		
+		VendorRepresentation vendRep = new VendorRepresentation(vendor);
+		return vendRep;
+	}
+	
+	public String removeVendor(Integer vendorId) {
+		
+		VendorManager.removeVendorById(vendorId);
+		
+		return "OK";
+	}
+	
+	/*public ProductRepresentation addVendorProduct(Integer vendorId, String productTitle, double productPrice) {
+		
+		Product product = VendorManager.addVendorProduct(vendorId, productTitle, productPrice)
+		
+		ProductRepresentation prodRep = new ProductRepresentation(product);
+		return prodRep;
+	}*/
 }
