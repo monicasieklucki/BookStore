915b5d3b9fdf145aeab5e8e19311e41dab151645
Monica Sieklucki <monica@Monicas-MacBook-Air.local>
2021-11-07 20:50:01 -0600

    changed url for server

--- src/com/ebook/server/EbookServer.java
+++ src/com/ebook/server/EbookServer.java
@@ -8,11 +8,15 @@ public class EbookServer {
 
 	public static void main(String[] args) {
 		JAXRSServerFactoryBean factoryBean = new JAXRSServerFactoryBean();
+        factoryBean.setResourceClasses(com.ebook.service.CustomerResource.class);
+        factoryBean.setResourceProvider(new SingletonResourceProvider(new com.ebook.service.CustomerResource()));
         factoryBean.setResourceClasses(com.ebook.service.vendor.resource.VendorResource.class);
         factoryBean.setResourceProvider(new SingletonResourceProvider(new com.ebook.service.vendor.resource.VendorResource()));
         factoryBean.setResourceClasses(com.ebook.service.order.resource.OrderResource.class);
         factoryBean.setResourceProvider(new SingletonResourceProvider(new com.ebook.service.order.resource.OrderResource()));
-        factoryBean.setAddress("http://localhost:8081/");
+        factoryBean.setAddress("http://book-store-luc.herokuapp.com/");
+        //http://book-store-luc.herokuapp.com/
+        //http://localhost:8081/
         Server server = factoryBean.create();
 
         System.out.println("HR Employee System Restful Server ready..............................."); 
