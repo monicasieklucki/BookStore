77c2fba6390caa3df9b50fef74223ffd1e903997
Monica Sieklucki <monica@Monicas-MacBook-Air.local>
2021-11-12 15:19:44 -0600

    using c3p0 JDBC connection pooling library

--- src/com/ebook/dal/DBHelper.java
+++ src/com/ebook/dal/DBHelper.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+  
 public class DBHelper {
 
 	public static Connection getConnection() {
@@ -29,9 +29,11 @@ public class DBHelper {
 		Connection connection = null;
  
 		try {
-		    String dbUrl = "jdbc:postgresql://ec2-54-236-137-173.compute-1.amazonaws.com:5432/dc37f4f559s4o8?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-		    System.out.println("URL " + dbUrl);
-			connection = DriverManager.getConnection(dbUrl, "vleimbeveodmce", "e31a27af1ef200c44c5d9e43c07eac442ba3b202650eeb90a719653b311d3cce");
+			
+            connection = C3PO.comboPooledDataSource.getConnection();
+		    //String dbUrl = "jdbc:postgresql://ec2-54-236-137-173.compute-1.amazonaws.com:5432/dc37f4f559s4o8";
+		    //System.out.println("URL " + dbUrl);
+			//connection = DriverManager.getConnection(dbUrl, "vleimbeveodmce", "e31a27af1ef200c44c5d9e43c07eac442ba3b202650eeb90a719653b311d3cce");
  
 
 				Statement st = connection.createStatement();
